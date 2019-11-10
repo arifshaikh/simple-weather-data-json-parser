182251998f8731492e66dbf2e064a3e4ab02b243
Arif Shaikh <arif.shaikh1@veritas.com>
2019-11-10 23:41:58 +0530

    Adds documentation and logging statements

--- src/main/java/com/example/jsonparser/service/RestServiceImpl.java
+++ src/main/java/com/example/jsonparser/service/RestServiceImpl.java
@@ -17,14 +17,20 @@ public class RestServiceImpl implements RestService {
 
     private static final Logger logger = LogManager.getLogger(RestServiceImpl.class.getName());
 
+    /**
+     * Implements getWeatherData of RestService interface
+     * @param url url to fetch data from
+     * @return WeatherMap instance
+     */
     @Override
     public WeatherMap getWeatherData(String url) {
         StringBuilder response = new StringBuilder();
         ObjectMapper objectMapper = new ObjectMapper();
         // Don't fail in deserialization on unknown properties
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        WeatherMap weatherMap = null;
+        WeatherMap weatherMap;
         try {
+            logger.debug("Opening connection to REST API URL: " + url);
             URL restUrl = new URL(url);
             HttpURLConnection conn = (HttpURLConnection) restUrl.openConnection();
             conn.setRequestMethod("GET");
@@ -35,6 +41,7 @@ public class RestServiceImpl implements RestService {
                 logger.error(message);
                 throw new RuntimeException(message);
             }
+            // Successfully received the response from API
             BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
             String output;
             while ((output = br.readLine()) != null) {
@@ -43,11 +50,10 @@ public class RestServiceImpl implements RestService {
             }
             // disconnect the connection to the URL
             conn.disconnect();
-
+            logger.debug("Received response from API, deserializing it");
             weatherMap = objectMapper.readValue(response.toString(), WeatherMap.class);
-            logger.debug(weatherMap);
         } catch (MalformedURLException e) {
-            String message = "Received MalformedURLException";
+            String message = "Received MalformedURLException, check URL";
             logger.error(message);
             throw new RuntimeException(message);
         } catch (IOException e) {
